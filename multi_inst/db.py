35fd95a8ed62c127b1f8958c25a412313f2ed500
Alexey-t <support@uvviewsoft.com>
2019-04-21 12:10:39 +0300

    multiinst

--- multi_inst/db.py
+++ multi_inst/db.py
@@ -281,6 +281,7 @@ PLUGINS={
             'FTP',
             'Hash_Generator',
             'Highlight_Occurrences',
+            'IP_Address_Helper',
             'Macros',
             'Numbered_Bookmarks',
             'Number_Utils',
