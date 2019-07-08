34787822ad1c2068e02f1f7737058c8fe2b50cb6
Alexey-t <support@uvviewsoft.com>
2019-07-09 00:46:08 +0300

    db

--- multi_inst/db.py
+++ multi_inst/db.py
@@ -87,7 +87,6 @@ PLUGINS={
             'HTML_XML',
             ),
         T_OTHER:(
-            'HTML_Beautify',
             'HTML_Live_Preview',
             'HTML_Ops',
             'HTML_Tooltips',
